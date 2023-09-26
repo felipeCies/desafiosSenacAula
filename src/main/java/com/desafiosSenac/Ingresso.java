51ab11cb900c97c818b78e099dc156b03cb1a9f2
felipeCies <cieslick246@gmail.com>
2023-09-26 13:33:40 -0300

    att

--- src/main/java/com/desafiosSenac/Ingresso.java
+++ src/main/java/com/desafiosSenac/Ingresso.java
@@ -6,8 +6,8 @@ public class Ingresso {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String nome, estudante;
-        int idade, ingresso;
+        String nome;
+        int idade;
 
         System.out.println("Qual seu nome?");
         nome = scanner.nextLine();
@@ -15,26 +15,39 @@ public class Ingresso {
         System.out.println("Qual a sua idade?");
         idade = scanner.nextInt();
 
+        scanner.nextLine();
+
         if (idade >= 18){
             System.out.println("você estuda python? Responda com s(sim) ou n(não)");
+            String estudante;
             estudante = scanner.nextLine();
 
-            scanner.nextLine();
 
             System.out.println("Você deseja o ingresso VIP ou Padrão?" + "\n1-Padrão: R$50.000" +"\n2-VIP: R$100.00");
-            ingresso = scanner.nextInt();
+            String ingresso;
+            ingresso = scanner.nextLine();
 
-            if (estudante.equalsIgnoreCase("S")) {
-                ingresso = ingresso/2;
+            if (estudante.equalsIgnoreCase("s") && ingresso.equalsIgnoreCase("1")) {
 
                 System.out.println("============================================");
                 System.out.println("Comprovante de reserva de ingresso:" +"\n"+ nome +"\n Seu ingresso " +
-                        " com desconto foi reservado!" + "\n valor do ingresso: " + "R$" +ingresso);
-            } else {
+                        " com desconto foi reservado!" + "\n valor do ingresso: " + "R$25.00");
+
+            } else if (estudante.equalsIgnoreCase("s") && ingresso.equalsIgnoreCase("1")){
                 System.out.println("============================================");
                 System.out.println("Comprovante de reserva de ingresso:" +"\n"+ nome +"\n Seu ingresso foi reservado!"
-                        + "\n valor do ingresso: " + "R$" +ingresso);
+                        + "\n valor do ingresso: " + "R$50.00");
 
+            } else if (estudante.equalsIgnoreCase("s") && ingresso.equalsIgnoreCase("2")){
+                System.out.println("============================================");
+
+                System.out.println("Comprovante de reserva de ingresso:" +"\n"+ nome +"\n Seu ingresso com desconto foi reservado!"
+                        + "\n valor do ingresso: " + "R$50.00");
+
+            }else if (estudante.equalsIgnoreCase("n") && ingresso.equalsIgnoreCase("2")){
+                System.out.println("============================================");
+                System.out.println("Comprovante de reserva de ingresso:" +"\n"+ nome +"\n Seu ingresso foi reservado!"
+                        + "\n valor do ingresso: " + "R$100.00");
             }
         }
     }
